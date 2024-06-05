9e8cf5559d82ac5e706ea415e87ec695b8b71a31
MjasonF <marcellinusjasonkjhs@gmail.com>
2024-06-05 13:11:09 +0700

    sqlite -> postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_06_03_064832) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.string "body"
     t.datetime "created_at", null: false
